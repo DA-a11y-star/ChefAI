60870d038c57b39318066cdaf147a3f52bea81f3
DA-a11y-star <87615327+DA-a11y-star@users.noreply.github.com>
2024-10-16 17:40:16 -0500

    test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-openai.api_key = "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"
+openai.api_key = ""
 app.secret_key = "supersecretkey"
 
 # THis is a test
